f45e214b554308c1a2677b6ade794b41b6c852fe
Highpower143 <nandyadu1c@gmail.com>
2025-01-27 19:03:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "29893020"
-API_HASH = "28e79037f0b334ef0503466c53f08af5"
+API_ID = "22180072"
+API_HASH = "425c7b0d42d9aa0f0b69c07687787175"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", 6399386263))
+OWNER_ID = int(getenv("OWNER_ID", 1786683163))
 SUPPORT_GRP = "NYCreation_Chatzone"
 UPDATE_CHNL = "CreativeYdv"
 OWNER_USERNAME = "Yo_Mysterious"
